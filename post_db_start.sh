810f542f7e6a5858810cc399b263499ada9b2d1f
João Guilherme <joao.sczip@hotmail.com>
2026-03-26 08:07:24 -0300

    rename containers

--- post_db_start.sh
+++ post_db_start.sh
@@ -2,5 +2,5 @@ docker exec \
     -it $PG_DB_CONTAINER \
     psql \
     -U root \
-    -d ecomm-be \
+    -d outbox \
     -c "SELECT * FROM pg_create_logical_replication_slot('${OUTBOX_REPLICATION_SLOT}', 'wal2json');"
